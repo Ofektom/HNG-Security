d81f2976f1d84edbb4e5dca6f729143f7e99d4d2
ofektom <okpohot@gmail.com>
2024-07-10 13:25:33 +0100

    PingTask for reload

--- src/main/java/com/example/HNG_Security/HngSecurityApplication.java
+++ src/main/java/com/example/HNG_Security/HngSecurityApplication.java
@@ -2,8 +2,10 @@ package com.example.HNG_Security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class HngSecurityApplication {
 
 	public static void main(String[] args) {
